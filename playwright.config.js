0dfa59c1d2b8488d6f6fb17b737059bb9c22f265
Vivek Brahmadandi <102134859+vbrahmadandi@users.noreply.github.com>
2025-01-14 22:40:36 +0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -5,4 +5,18 @@ module.exports = defineConfig({
     video: 'on', // Options: 'on', 'retain-on-failure', 'off', 'on-first-retry'
   },
   outputDir: 'test-results/', // Directory where videos and traces will be saved
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' }, // Use Chromium
+    },
+    {
+      name: 'firefox',
+      use: { browserName: 'firefox' }, // Use Firefox
+    },
+    {
+      name: 'webkit',
+      use: { browserName: 'webkit' }, // Use WebKit
+    },
+  ],
 });
