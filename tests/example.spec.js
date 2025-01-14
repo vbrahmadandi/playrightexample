0f014f06cce3eb57604f3b1d5ef718187a5b0bb4
Vivek Brahmadandi <102134859+vbrahmadandi@users.noreply.github.com>
2025-01-14 21:58:41 +0400

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example Domain/);
+  await page.goto('https://demo.alefed.com/');
+  const loginButton = await page.getByRole('button', { name: 'Login' });
+  await expect(loginButton).toBeVisible();
+  
+ // await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('has specific text', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page.locator('h1')).toHaveText('Example Domain');
+  await page.goto('https://moe.alefed.com');
+  //await expect(page.locator('h1')).toHaveText('Example Domain');
+   const loginButton = await page.getByRole('button', { name: 'Login' });
+  await expect(loginButton).toBeVisible();
 });
